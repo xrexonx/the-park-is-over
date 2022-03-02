dc95d26af91e392902bab8b82c58c290f4f6ebad
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2022-03-02 18:22:40 +0800

    Add high level pseudocode in readme and minor refactor for unpark service

--- README.md
+++ README.md
@@ -46,7 +46,40 @@ Cause our mouth just have run dra
     * vehicle_type
     * plate_number
     * entry_point
-  * unpark
-    * park_id
+  * un-park
+    * parking_id
     * exit_datetime
+
+### High level pseudocode
+* park
+  * Check and get nearest available slot by entry point and vehicle type
+    * if there is available slot
+      * Check if vehicle already parked and return within an hour
+        * if true
+          * Update the existing parking data with the new slot selected
+        * else
+          * Create new parking data
+        * end
+        * Update the slot to occupied
+        * Return parking data
+    * else
+      * Return no available slot message
+    * end
+      
+* un-park
+  * Get parking data by id
+    * if parking
+      * Check parking data if it has already a exit datetime value, which means the vehicle parked already and come back
+        * if parking has exit_datetime
+          * Get parking duration using the exit datetime and the new exit datetime
+        * else
+          * Get parking duration using the entry datetime and exit datetime
+        * end
+        * Compute amount using duration * parking slot rate depends on the vehicle type
+        * Create new Payment record
+        * Update parking slot to available
+        * Return parking and payment information
+    * else
+      * Return no parking data found
+    * end
     
--- app/services/parking_service/un_park.rb
+++ app/services/parking_service/un_park.rb
@@ -1,45 +1,48 @@
 module ParkingService
   class UnPark
     def self.run(id, exit_datetime = nil)
+
+      # Get parking data by id
       parking = Parking.find_by_id(id)
 
-      if parking.present?
+      # return error message if no found
+      return { error: 'No parking data found' } unless parking.present?
 
-        prev_exit_datetime = parking.exit_datetime
-        prev_payment = nil
-        prev_payment = Payment.where(parking_id: parking.id).last if prev_exit_datetime.present?
+      # Check if it has already exit_datetime value, which means the vehicle parked already and come back
+      prev_exit_datetime = parking.exit_datetime
+      last_payment = nil
 
-        exit_datetime ||= DateService::CurrentDatetime.get
-        parking.update_column(:exit_datetime, exit_datetime)
+      # if it has exit_datetime get last payment record
+      last_payment = Payment.where(parking_id: parking.id).last if prev_exit_datetime.present?
 
-        entry_datetime = prev_payment ? prev_exit_datetime : parking.entry_datetime
-        duration = DateService::DateTimeDiff.get(entry_datetime, parking.exit_datetime)
+      exit_datetime ||= DateService::CurrentDatetime.get
+      parking.update_column(:exit_datetime, exit_datetime)
 
-        slot = parking.slot
-        rate = slot.rate
-        amount = prev_payment ? duration * rate : PaymentService::Compute.run(duration, rate)
-        payment = {
-          duration: duration,
-          amount: amount,
-          parking: parking
-        }
+      entry_datetime = last_payment ? prev_exit_datetime : parking.entry_datetime
+      duration = DateService::DateTimeDiff.get(entry_datetime, parking.exit_datetime)
 
-        Payment.create!(payment)
-        slot.update_column(:status, Slot::AVAILABLE)
+      slot = parking.slot
+      rate = slot.rate
+      amount = last_payment ? duration * rate : PaymentService::Compute.run(duration, rate)
+      payment = {
+        duration: duration,
+        amount: amount,
+        parking: parking
+      }
 
-        {
-          entry_datetime: parking.entry_datetime,
-          exit_datetime: parking.exit_datetime,
-          base_rate: Parking::BASE_RATE.to_f,
-          slot_hourly_rate: rate.to_f,
-          duration: duration,
-          amount: amount.to_f,
-          slot: slot.name,
-          vehicle: parking.plate_number
-        }
-      else
-        { error: 'No parking data found' }
-      end
+      Payment.create!(payment)
+      slot.update_column(:status, Slot::AVAILABLE)
+
+      {
+        entry_datetime: parking.entry_datetime,
+        exit_datetime: parking.exit_datetime,
+        base_rate: Parking::BASE_RATE.to_f,
+        slot_hourly_rate: rate.to_f,
+        duration: duration,
+        amount: amount.to_f,
+        slot: slot.name,
+        vehicle: parking.plate_number
+      }
 
     end
   end
